29f1005e49aea4dbd1759e71f281335ea647b8b1
angelaslinker <angela.r.slinker@gmail.com>
2022-10-15 15:55:56 -0600

    finalziation of app

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import ClothingList from './components/ClothingList';
+
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <ClothingList />
+
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -14,7 +22,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+
 });
